2eb2033567c2bfe44dd6cc9ceac9fa0b3dc58d16
LucasTeodoro2003 Teodoro <lucasteodoro.lm@gmail.com>
2024-10-29 00:30:01 +0000

    update bug

--- src/widgets/backGround/ui/newBackgroundHome.tsx
+++ src/widgets/backGround/ui/newBackgroundHome.tsx
@@ -14,7 +14,7 @@ const NewbackgroundHome: React.FC<NewBackgroundHomeProps> = ({
   return (
     <div className="flex h-screen ml-64">
       <div className="bg-white dark:bg-gray-800 w-full h-full">
-        <div className="flex mt-5 mx-5 bg-white dark:bg-gray-800">
+        <div className="flex mt-5 bg-white dark:bg-gray-800">
           <div className="flex text-justify w-full justify-center items-center">
             <TableLocation address={address} user={user} />
           </div>
